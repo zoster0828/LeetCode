7ef36fda78694b024a4a8cc03973369633b9279a
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2023-07-04 23:23:19 +0900

    Time: 5 ms (85.14%), Space: 65.7 MB (28.78%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -1,30 +1,31 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public int pairSum(ListNode head) {
-        List<Integer> list = new ArrayList();
-        while(head != null) {
-            list.add(head.val);
-            head = head.next;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        // Get middle of the linked list.
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
         }
-        
-        int max = 0;
-        for(int i = 0 ; i < list.size() / 2 ; i++) {
-            int sum = list.get(i);
-            sum += list.get(list.size() - i -1);
-            if(sum > max) {
-                max = sum;
-            }
+
+        // Reverse second half of the linked list.
+        ListNode nextNode, prev = null;
+        while (slow != null) {
+            nextNode = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = nextNode;
         }
-        
-        return max;
+
+        ListNode start = head;
+        int maximumSum = 0;
+        while (prev != null) {
+            maximumSum = Math.max(maximumSum, start.val + prev.val);
+            prev = prev.next;
+            start = start.next;
+        }
+
+        return maximumSum;
     }
 }
