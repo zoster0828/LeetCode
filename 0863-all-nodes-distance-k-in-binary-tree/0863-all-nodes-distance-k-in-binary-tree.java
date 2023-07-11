eca1b949384e5f608eae3a20cadeef70d1204461
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2023-07-11 23:55:42 +0900

    Time: 13 ms (45.52%), Space: 41.3 MB (96.49%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -8,58 +8,69 @@
  * }
  */
 class Solution {
-    List<Integer> result = new ArrayList();
-    Set<TreeNode> visit = new HashSet();
-    Map<Integer, TreeNode> parentMap = new HashMap();
+    public void getParentsByBFS(Map<TreeNode, TreeNode> parent, TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            while (n-- > 0) {
+                TreeNode curr = queue.poll();
+                if (curr.left != null) {
+                    parent.put(curr.left, curr);
+                    queue.offer(curr.left);
+                }
+                if (curr.right != null) {
+                    parent.put(curr.right, curr);
+                    queue.offer(curr.right);
+                }
+            }
+        }
+    }
+
+    public void getParentsByDFS(Map<TreeNode, TreeNode> parent, TreeNode root) {
+        if (root == null)
+            return;
+        if (root.left != null)
+            parent.put(root.left, root);
+        if (root.right != null)
+            parent.put(root.right, root);
+        getParentsByDFS(parent, root.left);
+        getParentsByDFS(parent, root.right);
+    }
+
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        makeParentsMap(null, root, target);
-        findChilds(target, k);
-        findParents(target, k-1);
-        return result;
-    }
-
-    private void findParents(TreeNode node, int k) {
-        TreeNode parent = parentMap.get(node.val);
-        if(parent == null) return;
-        if(k == 0) {
-            result.add(parent.val);
-        }else {
-            findParents(parent, k - 1);
-            if (node != parent.left)
-                findChilds(parent.left, k - 1);
-            if (node != parent.right)
-                findChilds(parent.right, k - 1);
+        Map<TreeNode, TreeNode> parent = new HashMap<>();
+        getParentsByDFS(parent, root);
+        Queue<TreeNode> queue = new LinkedList<>();
+        Map<TreeNode, Boolean> visited = new HashMap<>();
+        visited.put(target, true);
+        queue.offer(target);
+        int level = 0;
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            if (level == k)
+                break;
+            level++;
+            while (n-- > 0) {
+                TreeNode curr = queue.poll();
+                if (curr.left != null && !visited.containsKey(curr.left)) {
+                    visited.put(curr.left, true);
+                    queue.offer(curr.left);
+                }
+                if (curr.right != null && !visited.containsKey(curr.right)) {
+                    visited.put(curr.right, true);
+                    queue.offer(curr.right);
+                }
+                if (parent.containsKey(curr) && !visited.containsKey(parent.get(curr))) {
+                    visited.put(parent.get(curr), true);
+                    queue.offer(parent.get(curr));
+                }
+            }
         }
-    }
-
-    void findChilds(TreeNode node, int k) {
-        if(node == null) return;
-        if(visit.contains(node)) return;
-
-        visit.add(node);
-
-        if(k==0) {
-            result.add(node.val);
-        } else {
-            findChilds(node.left, k-1);
-            findChilds(node.right, k-1);
+        List<Integer> ans = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            ans.add(queue.poll().val);
         }
-    }
-
-    void makeParentsMap(TreeNode parent, TreeNode node, TreeNode target) {
-        if(node == null) {
-            return;
-        }
-
-        if(parent != null) {
-            parentMap.put(node.val, parent);
-        }
-
-        if(node.val == target.val) {
-            return;
-        }
-
-        makeParentsMap(node, node.left, target);
-        makeParentsMap(node, node.right, target);
+        return ans;
     }
 }
