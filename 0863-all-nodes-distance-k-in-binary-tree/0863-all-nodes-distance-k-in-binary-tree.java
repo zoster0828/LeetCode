7a583c661571944e641a13bfd0c135be5e249881
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2023-07-11 23:51:05 +0900

    Time: 13 ms (45.52%), Space: 41.6 MB (80.75%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -8,53 +8,58 @@
  * }
  */
 class Solution {
-    public void getAllNodesFromK(TreeNode root,int k,List<Integer> res){
-        if(root==null){
-            return ;
-        }
-        if(k==0){
-            res.add(root.val);
-            return ;
-        }
-        getAllNodesFromK(root.left,k-1,res);
-        getAllNodesFromK(root.right,k-1,res);
-
-         
-    }
-    public int solve(TreeNode root, TreeNode target, int k,List<Integer> res){
-        if(root==null){
-            return -1;
-        }
-        if(root==target){
-            getAllNodesFromK(root,k,res);
-            return 1;
-        }
-        int left = solve(root.left,target,k,res);
-        int right = solve(root.right,target,k,res);
-        if(left!=-1 && left<=k){
-            if(left==k){
-                res.add(root.val);
-            }
-            else{
-                getAllNodesFromK(root.right,k-left-1,res);
-            }
-            return 1+left;
-        }
-        if(right!=-1 && right<=k){
-            if(right==k){
-                res.add(root.val);
-            }
-            else{
-                getAllNodesFromK(root.left,k-right-1,res);
-            }
-            return 1+right;
-        }
-        return -1;
-
-    }
+    List<Integer> result = new ArrayList();
+    Set<TreeNode> visit = new HashSet();
+    Map<Integer, TreeNode> parentMap = new HashMap();
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        List<Integer> res=new ArrayList<>();
-        solve(root,target,k,res);
-        return res;
+        makeParentsMap(null, root, target);
+        findChilds(target, k);
+        findParents(target, k-1);
+        return result;
+    }
+
+    private void findParents(TreeNode node, int k) {
+        TreeNode parent = parentMap.get(node.val);
+        if(parent == null) return;
+        if(k == 0) {
+            result.add(parent.val);
+        }else {
+            findParents(parent, k - 1);
+            if (node != parent.left)
+                findChilds(parent.left, k - 1);
+            if (node != parent.right)
+                findChilds(parent.right, k - 1);
+        }
+    }
+
+    void findChilds(TreeNode node, int k) {
+        if(node == null) return;
+        if(visit.contains(node)) return;
+
+        visit.add(node);
+
+        if(k==0) {
+            result.add(node.val);
+        } else {
+            findChilds(node.left, k-1);
+            findChilds(node.right, k-1);
+        }
+    }
+
+    void makeParentsMap(TreeNode parent, TreeNode node, TreeNode target) {
+        if(node == null) {
+            return;
+        }
+
+        if(parent != null) {
+            parentMap.put(node.val, parent);
+        }
+
+        if(node.val == target.val) {
+            return;
+        }
+
+        makeParentsMap(node, node.left, target);
+        makeParentsMap(node, node.right, target);
     }
 }
