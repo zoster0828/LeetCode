84883ae48dd0f1c68ba191656b366ee8042e8fa5
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2023-07-16 16:27:48 +0900

    Time: 0 ms (100.00%), Space: 39.7 MB (96.71%) - LeetHub

--- 0984-string-without-aaa-or-bbb/0984-string-without-aaa-or-bbb.java
+++ 0984-string-without-aaa-or-bbb/0984-string-without-aaa-or-bbb.java
@@ -1,53 +1,48 @@
 class Solution {
     public String strWithout3a3b(int a, int b) {
-        String result = "";
-        String before = "";
-        int continuity = 0;
-        while(a != b) {
-            if(a > b) {
-                if(before == "a"){
-                    if(continuity != 2) {                    
-                        result += "a";
-                        a--;
-                        continuity++;
-                    } else {
-                        result += "b";
-                        b--;
-                        continuity = 0;
-                        before = "b";
-                    }
-                } else {
-                    result += "a";
-                    before = "a";
-                    a--;
-                    continuity++;
-                }
-            } else {
-                if(before == "b"){
-                    if(continuity != 2) {                    
-                        result += "b";
-                        b--;
-                        continuity++;
-                    } else {
-                        result += "a";
-                        a--;
-                        continuity = 0;
-                        before = "a";
-                    }
-                } else {
-                    result += "b";
-                    before = "b";
-                    b--;
-                    continuity++;
-                }
-            }
+            StringBuilder result = new StringBuilder();
+    char charA = 'a', charB = 'b';
+    int countA = 0, countB = 0;
+
+    if (b > a) {
+        int tempCount = a;
+        a = b;
+        b = tempCount;
+
+        char tempChar = charA;
+        charA = charB;
+        charB = tempChar;
+    }
+
+    while (a > 0 || b > 0) {
+        // Append 'a' if remaining 'a' count is greater than 'b' count and consecutive 'a's are less than 2
+        if (a > b && countA < 2) {
+            result.append(charA);
+            countA++;
+            countB = 0;
+            a--;
         }
-        
-        if(a==b) {
-            result += one(a, before);
+        // Append 'b' if remaining 'b' count is greater than 0 and consecutive 'b's are less than 2
+        else if (b > 0 && countB < 2) {
+            result.append(charB);
+            countB++;
+            countA = 0;
+            b--;
         }
-        
-        return result;
+
+        // Swap characters if 'b' count is greater than 'a' count
+        if (b > a) {
+            int tempCount = a;
+            a = b;
+            b = tempCount;
+
+            char tempChar = charA;
+            charA = charB;
+            charB = tempChar;
+        }
+    }
+
+    return result.toString();
     }
     
     String one(int count, String start) {
