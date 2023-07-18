72c4b17dc08fb4d9592d4d54a19aee3cde2f62d9
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2023-07-18 22:29:47 +0900

    Time: 95 ms (6.53%), Space: 124.9 MB (23.09%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.java
@@ -0,0 +1,98 @@
+class LRUCache {
+    private int capacity;
+    Map<Integer, Value> map;
+    private int time;
+    Queue<Key> queue;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.map = new HashMap();
+        this.time = 0;
+        this.queue = new LinkedList();
+    }
+
+    public int get(int key) {
+        time++;
+        Value value = map.get(key);
+        if(value == null) {
+            return -1;
+        }
+        add(key, value.getValue(), time);
+        int result = map.get(key).getValue();
+        return result;
+    }
+
+    public void put(int key, int value) {
+        time++;
+        if(map.containsKey(key)) {
+            add(key, value, time);
+        } else {
+            if(capacity == map.size()) {
+                evict();
+            }
+        }
+
+        add(key, value, time);
+    }
+
+    public void evict() {
+        Key key = queue.poll();
+        Value value = map.get(key.getKey());
+        if(value == null) {
+            evict();
+            return;
+        }
+        if(key.getTime() == value.getTime())
+            map.remove(key.getKey());
+        else {
+            evict();
+        }
+    }
+    
+    private void add(int key, int value, int time) {
+        map.put(key, new Value(value, time));
+        queue.add(new Key(key, time));
+    }
+
+    class Value{
+        private int value;
+        private int time;
+        public Value(int value, int time) {
+            this.value = value;
+            this.time = time;
+        }
+
+        public void update(int time) {
+            this.time = time;
+        }
+
+        public int getValue() {
+            return this.value;
+        }
+
+        public int getTime() {
+            return this.time;
+        }
+    }
+
+    class Key{
+        private int key;
+        private int time;
+        public Key(int key, int time) {
+            this.key = key;
+            this.time = time;
+        }
+
+        public void update(int time) {
+            this.time = time;
+        }
+
+        public int getKey() {
+            return this.key;
+        }
+
+        public int getTime() {
+            return this.time;
+        }
+    }
+}
