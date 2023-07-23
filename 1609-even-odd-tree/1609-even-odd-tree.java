4f6902032a51a2bbf2abae631a40c79fea40cbe5
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2023-07-23 23:47:48 +0900

    Time: 25 ms (22.05%), Space: 79.2 MB (6.01%) - LeetHub

---
+++ 1609-even-odd-tree/1609-even-odd-tree.java
@@ -0,0 +1,72 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Queue<TreeNode> queue = new LinkedList();
+    List<Integer> lv = new ArrayList();
+    public boolean isEvenOddTree(TreeNode root) {
+        queue.add(root);        
+        return bfs(false, 1);
+    }
+    public boolean bfs(boolean odd, int count) {
+        int next = 0;
+        List<Integer> now = new ArrayList();
+        while(count > 0){
+            count--;
+            TreeNode node = queue.poll();
+            if(!odd) {
+                if(node.val % 2 != 1) return false;
+            } else {
+                if(node.val % 2 != 0) return false;
+            }
+            now.add(node.val);
+            if(node != null){
+                if(node.left != null) {
+                    queue.add(node.left);
+                    next++;
+                }
+                
+                if(node.right != null) {
+                    queue.add(node.right);
+                    next++;
+                }
+            }            
+        }
+        if(odd) {            
+            int curr = Integer.MAX_VALUE;
+            for(int i = 0 ; i < now.size() ; i++) {
+                if(curr > now.get(i)) {
+                    curr = now.get(i);
+                } else {
+                    return false;
+                }
+            }
+        } else {
+            int curr = Integer.MIN_VALUE;
+            for(int i = 0 ; i < now.size() ; i++) {
+                if(curr < now.get(i)) {
+                    curr = now.get(i);
+                } else {
+                    return false;
+                }
+            }
+        }
+        if(queue.isEmpty()) {
+            return true;
+        } else {
+            return bfs(!odd, next);
+        }
+    }
+}
