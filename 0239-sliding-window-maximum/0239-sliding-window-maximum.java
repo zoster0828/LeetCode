7adfc12f7b4476d6486a7099f98e76dd07778543
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2023-08-17 00:37:26 +0900

    Time: 215 ms (10.60%), Space: 61.6 MB (30.24%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        PriorityQueue<Value> queue = new PriorityQueue();
+        for (int i = 0; i < k; i++) {
+            queue.add(new Value(nums[i], i));
+        }
+
+        int[] result = new int[nums.length - k + 1];
+
+        for (int i = 0; i < nums.length - k + 1; i++) {
+            queue.add(new Value(nums[i+k-1], i+k-1));
+            Value value = null;
+            while(true) {
+                value = queue.poll();
+                if (value.position < i || value.position >= i + k) {
+                    continue;
+                }
+                break;
+            }
+            result[i] = value.value;
+            queue.add(value);
+        }
+
+        return result;
+    }
+
+    public static class Value implements Comparable<Value>{
+        int value;
+        int position;
+
+        public Value(int value, int position) {
+            this.value = value;
+            this.position = position;
+        }
+
+        @Override
+        public int compareTo(Value o) {
+            return o.value >= this.value ? 1 : -1;
+        }
+    }
+}
