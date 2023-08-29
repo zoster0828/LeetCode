25ae685842fef3715eded1ce392086b039ccc379
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2023-08-29 21:59:29 +0900

    Time: 9 ms (91.25%), Space: 43.7 MB (99.66%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
@@ -1,28 +1,17 @@
 class Solution {
-    public int bestClosingTime(String customers) {  
-        char[] chars = customers.toCharArray();
-        int open = 0;
-        for (char aChar : chars) {
-            if(isOpened(aChar)) {
-                open++;
-            }
-        }
-
-        int minValue = open;
+    public int bestClosingTime(String customers) {
+        int maxValue = 0;
         int minValuePosition = 0;
-        int[] values = new int[chars.length+1];
-        values[0] = open;
-        int closed = 0;
-        for (int i = 0; i < chars.length; i++) {
-            if(isOpened(chars[i])) {
-                open--;
+        int currentValue = 0;
+        for (int i = 0; i < customers.length(); i++) {
+            if(isOpened(customers.charAt(i))) {
+                currentValue++;
             } else {
-                closed++;
+                currentValue--;
             }
-            values[i+1] = open+closed;
 
-            if(values[i+1] < minValue) {
-                minValue = values[i+1];
+            if(currentValue > maxValue) {
+                maxValue = currentValue;
                 minValuePosition = i+1;
             }
         }
@@ -30,7 +19,7 @@ class Solution {
         return minValuePosition;
     }
 
-    private static boolean isOpened(char chars) {
+    private boolean isOpened(char chars) {
         return chars == 'Y';
     }
 }
