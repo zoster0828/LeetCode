653634f4d8c102f6ed5fb6e5161e19631f9382c7
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2023-09-06 23:17:21 +0900

    Time: 0 ms (100.00%), Space: 43.8 MB (27.02%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
@@ -11,40 +11,49 @@
 class Solution {
     public ListNode[] splitListToParts(ListNode head, int k) {
         ListNode[] result = new ListNode[k];
-        ListNode target = head;
-        
-        int length = 0 ;
-        while(target != null) {            
-            target = target.next;
-            length ++;
+        ListNode temp = head;
+
+        int lengthOfListNode = getLengthOfListNode(temp) ;
+
+        int quotient = lengthOfListNode / k;
+        int remainder = lengthOfListNode % k;
+        for(int multiple = 0 ; multiple < k ; multiple++) {
+            result[multiple] = head;
+            int startPos = quotient*multiple;
+            int endPos = quotient*(multiple+1)+(remainder-- > 0 ? 1 : 0);
+            int nodeLength = endPos - startPos -1;
+            head = moveToNextHead(head, nodeLength);
         }
-        
-        int split = length / k;
-        int nam = length % k;
-        for(int i = 0 ; i < result.length ; i++) {
-            result[i] = head;
-            head = move(head, split*i, split*(i+1)+(nam-- > 0 ? 1 : 0));            
-            if(head != null) {
-                ListNode temp = head;
-                head = head.next;    
-                temp.next = null;
-            }
-        }
-        
+
         return result;
     }
-    
-    private ListNode move(ListNode node, int move, int cut) {
-        
+
+    private int getLengthOfListNode(ListNode temp) {
+        int lengthOfListNode = 0;
+        while(temp != null) {
+            temp = temp.next;
+            lengthOfListNode++;
+        }
+        return lengthOfListNode;
+    }
+
+    private ListNode moveToNextHead(ListNode node, int nodeLength) {
+
         ListNode target = node;
-        
-        for(int i = 0 ; i <  cut-move-1 ; i++) {
+
+        for(int i = 0 ; i <  nodeLength ; i++) {
             if(target == null) {
                 break;
             }
             target = target.next;
-        }        
-        
+        }
+
+        if(target != null) {
+            ListNode temp = target;
+            target = target.next;
+            temp.next = null;
+        }
+
         return target;
     }
 }
