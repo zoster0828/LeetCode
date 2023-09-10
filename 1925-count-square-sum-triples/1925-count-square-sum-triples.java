3b771985c5df7dafc286b3bb7b369438a8ed2447
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2023-09-10 23:03:23 +0900

    Time: 128 ms (30.46%), Space: 39.3 MB (54.99%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.java
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.java
@@ -4,15 +4,16 @@ class Solution {
         for(int i = 1 ; i <= n ; i++) {
             int square = i*i;
             for(int j = 1 ; j < i ; j++) {
+                int a = j*j;
                 for(int k = 1 ; k < i ; k++) {
                     if(j == k) {
                         continue;
                     }
-                    if(square < j*j + k*k) {
+                    if(square < a + k*k) {
                         break;
                     }
                     
-                    if(square == j*j + k*k) {
+                    if(square == a + k*k) {
                         result++;
                     }
                 }
