0cabcdb6100d1d6465483c5d9723b1f80527635e
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2023-09-10 21:00:01 +0900

    Time: 0 ms (100.00%), Space: 39.2 MB (71.95%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
@@ -0,0 +1,13 @@
+class Solution {
+    int MOD = 1000000007;
+    public int countOrders(int n) {
+        long all = 1;
+        for(int i = 1 ; i <= n ; i++) {
+            all *= i;
+            all *= (2*i-1);
+            all %= MOD;
+        }
+
+        return (int)all;
+    }
+}
