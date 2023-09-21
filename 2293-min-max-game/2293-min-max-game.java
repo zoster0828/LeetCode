0aabe96866158f536e4e43f96756334aa7d34913
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2023-09-21 17:54:46 +0900

    Time: 1 ms (56.88%), Space: 43 MB (50.83%) - LeetHub

--- 2293-min-max-game/2293-min-max-game.java
+++ 2293-min-max-game/2293-min-max-game.java
@@ -3,12 +3,8 @@ class Solution {
         while(nums.length != 1) {
             boolean odd = true;
             int[] temp = new int[nums.length / 2];
-            for(int i = 0 ; i < nums.length ; i+=2) {                
-                if(odd) {
-                   temp[i/2] = Math.min(nums[i], nums[i+1]);
-                } else {
-                    temp[i/2] = Math.max(nums[i], nums[i+1]);
-                }
+            for(int i = 0 ; i < nums.length ; i+=2) {                                
+                temp[i/2] = odd ? Math.min(nums[i], nums[i+1]) : Math.max(nums[i], nums[i+1]);                
                 odd = true ^ odd;
             }
             nums = temp;
