ada30bd34c29c6cdda895004da869423916be3b6
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2023-09-25 22:07:56 +0900

    Time: 1 ms (100.00%), Space: 40.7 MB (71.69%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -2,15 +2,17 @@ class Solution {
     public char findTheDifference(String s, String t) {
         char[] a1 = s.toCharArray();
         char[] a2 = t.toCharArray();
-        
-        Arrays.sort(a1); Arrays.sort(a2);
-        
+
+        int suma = 0;
         for(int i = 0 ; i < a1.length ; i ++) {
-            if(a1[i] != a2[i]) {
-                return a2[i];
-            }
+            suma += a1[i];
         }
-        
-        return a2[a2.length-1];
+
+        int sumb = 0;
+        for(int i = 0 ; i < a2.length ; i ++) {
+            sumb += a2[i];
+        }
+
+        return (char) (sumb-suma);
     }
 }
