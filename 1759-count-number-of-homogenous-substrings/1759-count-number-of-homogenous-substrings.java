fae241e5070b935e7482565eaa9eef8870aadf1e
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2023-11-09 23:28:10 +0900

    Time: 7 ms (96.40%), Space: 44 MB (66.80%) - LeetHub

---
+++ 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int countHomogenous(String s) {
+        int result = 0;
+        char prev = '1';
+        int continuous = 0;
+        for(char c : s.toCharArray()) {
+            if(prev != c) {
+                int fac = sum(continuous);
+                result += fac;
+                continuous = 1;
+                prev = c;
+            } else {
+                continuous += 1;
+            }
+        }
+        
+        int fac = sum(continuous);
+        result += fac;
+        
+        return result;
+    }
+    
+    int sum(int num) {
+        int result = 0;
+        for(int i = 1 ; i <= num ; i++) {
+            result += i;
+            result = result %1000000007;
+        }
+        
+        return result%1000000007;
+    }
+}
