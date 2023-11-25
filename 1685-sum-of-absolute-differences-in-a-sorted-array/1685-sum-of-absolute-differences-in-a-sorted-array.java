a1ac3da2006f18a7669f83fa1b939efb35c4ddad
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2023-11-25 10:25:31 +0900

    Time: 3 ms (99.36%), Space: 60.2 MB (5.77%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
@@ -1,30 +1,23 @@
 class Solution {
     public int[] getSumAbsoluteDifferences(int[] nums) {
-        int n = nums.length;
-        int[] result = new int[n];
-        int[] prefixSum = new int[n];
-        int[] suffixSum = new int[n];
+        int sum = 0;
+        for (int n : nums) {
+            sum += n;
+        }
+        int left = 0;
+        int right = sum;
 
-        // Calculate prefix sums
-        prefixSum[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            prefixSum[i] = prefixSum[i - 1] + nums[i];
+        int[] r = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+            right -= n;
+
+            r[i] = n * i - left + right - n * (nums.length - i - 1);
+
+            left += n;
         }
 
-        // Calculate suffix sums
-        suffixSum[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            suffixSum[i] = suffixSum[i + 1] + nums[i];
-        }
-
-        // Calculate the result in one pass
-        for (int i = 0; i < n; i++) {
-            int leftSum = i > 0 ? prefixSum[i - 1] : 0;
-            int rightSum = suffixSum[i] - nums[i];
-
-            result[i] = nums[i] * i - leftSum + rightSum - nums[i] * (n - i - 1);
-        }
-
-        return result;
+        return r;
     }
 }
