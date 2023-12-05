48c66ab662515316fedb61cef6b4573e1196bbb4
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2023-12-05 18:36:21 +0900

    Time: 0 ms (100.00%), Space: 39.3 MB (49.20%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numberOfMatches(int n) {
+        int result = 0;
+        
+        while(n != 1) {            
+            if(n%2 == 0) {                
+                n /= 2;
+                result += n;                
+                continue;
+            }
+            
+            n/=2;
+            result += n;
+            n += 1;
+        }
+        return result;
+    }
+}
