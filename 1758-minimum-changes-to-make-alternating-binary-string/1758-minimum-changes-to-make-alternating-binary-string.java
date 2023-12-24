7745551f02178bec451429eb5c519f945458db84
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2023-12-24 10:44:30 +0900

    Time: 3 ms (92.13%), Space: 42.9 MB (5.57%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -1,34 +1,28 @@
 class Solution {
     public int minOperations(String s) {
         char[] chars = s.toCharArray();
+        int odd[] = new int[2];
+        int even[] = new int[2];
         
-        int result = 0;
                 
         for(int i = 0 ; i < chars.length ; i++) {
-            if(i%2 == 0) {
-                if(chars[i] != '0') {
-                    result++;
+            if(i % 2 == 0) {
+                if(chars[i] == '1') {
+                    odd[1]++;                    
+                } else {
+                    even[1]++;
                 }
-            }else {
-                if(chars[i] != '1') {
-                    result++;
-                }
-            }
-        }
-        
-        int result2 = 0;
-        for(int i = 0 ; i < chars.length ; i++) {
-            if(i%2 == 1) {
-                if(chars[i] != '0') {
-                    result2++;
-                }
-            }else {
-                if(chars[i] != '1') {
-                    result2++;
+            } else {
+                if(chars[i] == '0') {
+                    odd[0]++;                    
+                } else {
+                    even[0]++;
                 }
             }
         }
         
-        return Math.min(result, result2);
+        int max = Math.max((odd[0]+odd[1]), (even[0]+even[1]));
+        
+        return chars.length - max;
     }
 }
