ca1f26f27b54b9033a1bea3ac6be03fc1a8e4b29
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-01-01 11:57:12 +0900

    Time: 8 ms (98.82%), Space: 45 MB (22.14%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        if(g.length == 0 || s.length == 0) return 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int chpo = 0;
+        int coopo = 0;
+        int result = 0;
+        
+        while(true) {
+            if(g[chpo] <= s[coopo]) {
+                result++;
+                chpo++;
+                coopo++;
+            } else {
+                coopo++;
+            }
+            
+            if(chpo == g.length || coopo == s.length) break;
+        }        
+        
+        
+        return result;
+    }
+}
