1f53197faa5660b49746f21a4feecd0c2b1f3173
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-01-04 22:56:12 +0900

    Time: 17 ms (46.49%), Space: 42.9 MB (71.35%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        paragraph = paragraph.toLowerCase().replaceAll("\\W+"," ");
+        String[] arr = paragraph.split("\\s+");
+
+        int n = arr.length;
+        HashMap<String,Integer> hp = new HashMap<>();
+        for(int i=0; i < n; i++){
+            hp.put(arr[i],hp.getOrDefault(arr[i],0)+1);
+        }
+
+        for(int i=0; i < banned.length; i++){
+            hp.remove(banned[i]);
+        }
+
+
+        int freq = 0;
+        String ans = "";
+        for(var a : hp.keySet()){
+            if(hp.get(a) > freq){
+                freq = hp.get(a);
+                ans = a;
+            }
+        }
+        return ans;
+    }
+}
