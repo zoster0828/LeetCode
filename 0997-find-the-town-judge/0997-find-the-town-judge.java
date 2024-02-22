08da4aa79325097fff43f8f16d3a3531cf12c30e
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-02-23 02:28:51 +0900

    Time: 3 ms (71.04%), Space: 54.3 MB (15.73%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -3,8 +3,10 @@ class Solution {
         int[] t = new int[n+1];
         
         for(int i = 0 ; i < trust.length ; i++) {
-            t[trust[i][1]]++;
             t[trust[i][0]]--;
+            if(t[trust[i][1]] < 0) continue;
+            t[trust[i][1]]++;
+            
         }
         
         for(int i = 1 ; i < n+1 ; i++) {
