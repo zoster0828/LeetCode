66f640694d9e969150f055e1b675106b7478912c
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-03-09 09:34:07 +0900

    Time: 0 ms (100.00%), Space: 62.4 MB (80.15%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -4,6 +4,8 @@ class Solution {
         int j = 0;
         int a = nums1.length;
         int b = nums2.length;
+        if(nums1[a-1] < nums2[0] || nums2[b-1] < nums1[0]) return -1;
+        
         while(i < a && j < b){
             if(nums1[i] == nums2[j]) {
                 return nums1[i];
