c669b6626cf704e8e9b4a66346798075af41d409
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-03-10 09:14:39 +0900

    Time: 1 ms (97.74%), Space: 43.5 MB (24.92%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        int[] one = new int[1001];
+        int[] two = new int[1001];
+        
+        for(int num : nums1) {
+            one[num]++;
+        }
+        
+        for(int num : nums2) {
+            two[num]++;
+        }
+        
+        boolean[] result = new boolean[1001];
+        
+        int size = 0;
+        for(int i = 0 ; i < 1001 ; i++) {
+            if(one[i] > 0 && two[i] > 0) {
+                result[i] = true;
+                size++;
+            }
+        }
+        
+        int[] r = new int[size];
+        
+        for(int i = 0, j =0 ; i < 1001 ; i++) {
+            if(result[i]) {
+                r[j] = i;
+                j++;
+            }
+        }
+        
+        return r;
+    }
+}
