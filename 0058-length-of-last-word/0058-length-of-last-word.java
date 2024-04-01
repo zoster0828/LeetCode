29bd09f95ec0007bb86bff2a4f0481986d4aaa9b
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-04-01 22:10:10 +0900

    Time: 0 ms (100.00%), Space: 41.4 MB (86.56%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,16 +1,12 @@
 class Solution {
     public int lengthOfLastWord(String s) {
+
         int continuous = 0;
-        int temp = 0;
-        for(int i = 0 ; i < s.length() ; i++) {
-            if(s.charAt(i) == ' ') {                
-                continuous = temp == 0 ? continuous : temp;
-                temp = 0;
-            } else {
-                temp++;
-            }
+        for(int i = s.length() - 1; i >= 0 ; i--) {
+            if(s.charAt(i) != ' ') continuous++;
+            else if(continuous != 0) break;
         }
         
-        return temp == 0 ? continuous : temp;
+        return continuous;
     }
 }
