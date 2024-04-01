54bff3d16113729862841cee6582931784d10644
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-04-01 22:07:55 +0900

    Time: 2 ms (13.54%), Space: 41.9 MB (18.81%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,6 +1,16 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] split = s.split("\\s+");
-        return split[split.length -1].length();
+        int continuous = 0;
+        int temp = 0;
+        for(int i = 0 ; i < s.length() ; i++) {
+            if(s.charAt(i) == ' ') {                
+                continuous = temp == 0 ? continuous : temp;
+                temp = 0;
+            } else {
+                temp++;
+            }
+        }
+        
+        return temp == 0 ? continuous : temp;
     }
 }
