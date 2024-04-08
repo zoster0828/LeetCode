27d1773c0c70dab27f23ec23f45116f63d933831
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-04-08 22:03:46 +0900

    Time: 0 ms (100.00%), Space: 40.8 MB (97.49%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -1,24 +1,42 @@
 class Solution {
-    public int countStudents(int[] students, int[] sandwiches) {
-        int i = 0;
-        int len = students.length;
-        int sandpo = 0;
-        int start = 0;
-        int eat = 0;
-        while(true) {
-            if(students[(i) % len] == sandwiches[sandpo]) {
-                sandpo++;   
-                students[(i) % len] = -1;
-                eat++;
-                start = (i) % len;
-            }
+//     public int countStudents(int[] students, int[] sandwiches) {
+//         int i = 0;
+//         int len = students.length;
+//         int sandpo = 0;
+//         int start = 0;
+//         int eat = 0;
+//         while(true) {
+//             if(students[(i) % len] == sandwiches[sandpo]) {
+//                 sandpo++;   
+//                 students[(i) % len] = -1;
+//                 eat++;
+//                 start = (i) % len;
+//             }
             
-            i++;
+//             i++;
             
-            if(start == (i) % len) {break;}
-            if(sandpo == sandwiches.length) return 0;
-        }
+//             if(start == (i) % len) {break;}
+//             if(sandpo == sandwiches.length) return 0;
+//         }
         
-        return len - eat;
+//         return len - eat;
+//     }
+    
+    public int countStudents(int[] students, int[] sandwiches) {
+        int[] count = new int[2];
+        for (int student : students) {
+            count[student]++;
+        }
+
+        for (int i = 0; i < sandwiches.length; i++) {
+            if (count[sandwiches[i]] > 0) {
+                count[sandwiches[i]]--;
+            } else {                
+                return sandwiches.length - i;
+            }
+        }
+
+        return 0;
     }
+
 }
