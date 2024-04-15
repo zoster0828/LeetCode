6891e3e9e5ab76a44f0030e72c8a0937e06940ef
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-04-15 15:39:53 +0900

    Time: 9 ms (7.69%), Space: 41.4 MB (17.41%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -16,25 +16,20 @@
 class Solution {
     Queue<TreeNode> q = new LinkedList();
     List<String> list = new ArrayList();
-    
+    int result = 0;
     public int sumNumbers(TreeNode root) {
         if(root == null) {return 0;}
         
         bfs("", root);
     
         
-        int result = 0;
-        for(int i = list.size() - 1 ; i >= 0 ; i--) {
-            result += Integer.parseInt(list.get(i));
-        }
-        
         return result;
     } 
     
     public void bfs(String value, TreeNode root) {
-        String v = value +""+ root.val;        
+        String v = value + root.val;        
         
-        if(root.left == null && root.right == null) list.add(v);
+        if(root.left == null && root.right == null) result += Integer.parseInt(v);
         
         if(root.left != null) {
             bfs(v, root.left);
