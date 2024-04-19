b0ffd2ae6eef96597ddba3441e052dd60bba39ad
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-04-19 10:48:18 +0900

    Time: 2 ms (99.65%), Space: 49.4 MB (57.87%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,9 +1,9 @@
 class Solution {
     public int numIslands(char[][] grid) {
         int count = 0;
-        for(int h = 0 ; h < grid.length ; h++){
-            for(int w = 0 ; w < grid[0].length ; w++){
-                if(grid[h][w] == '1'){
+        for(int w = 0 ; w < grid.length ; w++){
+            for(int h = 0 ; h < grid[0].length ; h++){
+                if(grid[w][h] == '1'){
                     count++;
                     find(grid, w, h);
                 }
@@ -14,23 +14,23 @@ class Solution {
     }
 
     public void find(char[][] grid, int w, int h) {
-        if(grid[h][w] == '0') {return;}
+        if(grid[w][h] == '0') {return;}
         
-        grid[h][w] = '0';
+        grid[w][h] = '0';
         
-        if(w > 0) {     
+        if(w > 0 && grid[w-1][h] == '1') {     
             find(grid, w-1, h);
         }
 
-        if(grid[0].length - 1 > w) {
-            find(grid, w+1, h);
-        }
-
-        if(grid.length - 1 > h) {
+        if(grid[0].length - 1 > h && grid[w][h+1] == '1') {
             find(grid, w, h+1);
         }
 
-        if(h > 0) {
+        if(grid.length - 1 > w && grid[w+1][h] == '1') {
+            find(grid, w+1, h);
+        }
+
+        if(h > 0 && grid[w][h-1] == '1') {
             find(grid, w, h-1);
         }
     }
