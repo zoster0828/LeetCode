e89f6545f0f09d42cd8fde998f6c28d14e56aedf
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-04-22 22:52:47 +0900

    Time: 8 ms (99.61%), Space: 14.4 MB (99.01%) - LeetHub

---
+++ 0752-open-the-lock/0752-open-the-lock.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int openLock(vector<string>& deadends, string target) {
+        if (target == "0000") return 0;
+        queue<int> queue;
+        queue.push(0);
+        bool seen[10000]{false};
+        for (auto& d : deadends)
+            seen[stoi(d)] = true;
+        int targ = stoi(target);
+        if (seen[0]) return -1;
+        for (int turns = 1; queue.size(); turns++) {
+            int qlen = queue.size();
+            for (int i = 0; i < qlen; i++) {
+                int curr = queue.front();
+                queue.pop();
+                for (int j = 1; j < 10000; j *= 10) {
+                    int mask = curr % (j * 10) / j,
+                        masked = curr - (mask * j);
+                    for (int k = 1; k < 10; k += 8) {
+                        int next = masked + (mask + k) % 10 * j;
+                        if (seen[next]) continue;
+                        if (next == targ) return turns;
+                        seen[next] = true;
+                        queue.push(next);
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+};
