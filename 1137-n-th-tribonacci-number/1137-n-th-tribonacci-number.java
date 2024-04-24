09b31fdca8c04d2ccc94c33a5be53ae5fe35c2b0
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-04-24 19:52:03 +0900

    Time: 0 ms (100.00%), Space: 40.2 MB (44.93%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,19 +1,19 @@
 class Solution {
-    Map<Integer, Integer> memo = new HashMap();
+    int[] memo = new int[100];
     public int tribonacci(int n) {
         return T(n);
     }
     
     public int T(int n) {
-        if(memo.containsKey(n)) {
-            return memo.get(n);
+        if(memo[n] != 0) {
+            return memo[n];
         }
         if(n == 0) return 0;
         if(n == 1) return 1;
         if(n == 2) return 1;
         
         int result = T(n-3) + T(n-2) + T(n-1);
-        memo.put(n, result);
+        memo[n] = result;
         return result;
     }
 }
