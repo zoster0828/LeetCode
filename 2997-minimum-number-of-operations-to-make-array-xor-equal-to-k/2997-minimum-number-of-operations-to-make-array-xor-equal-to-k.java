f64c21140b22de3a71fbf49afab69a7fef7dd5f8
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-04-29 22:43:37 +0900

    Time: 1 ms (100.00%), Space: 56.4 MB (59.59%) - LeetHub

--- 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.java
+++ 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.java
@@ -1,30 +1,13 @@
 class Solution {
     public int minOperations(int[] nums, int k) {
-        int[] count = new int[22];
-        
-        for(int i = 0 ; i < nums.length ; i++){
-            String str = Integer.toBinaryString(nums[i]);
-            for(int j = str.length()-1 ; j >= 0 ; j--) {
-                if(str.charAt(j) == '1') {
-                    count[-(j-str.length()-1)]++;
-                }
-            }
+        int sum = 0;
+
+        for (int num : nums) {
+            sum ^= num;
         }
         
-        String str = Integer.toBinaryString(k);
-        for(int j = str.length()-1 ; j >= 0 ; j--) {
-            if(str.charAt(j) == '1') {
-                count[-(j-str.length()-1)]++;
-            }
-        }
-        
-        
-        int result = 0;
-        for(int i : count) {
-            if(i % 2 != 0) {
-                result++;
-            }
-        }
-        return result;
+        sum ^= k;
+
+        return Integer.bitCount(sum);
     }
 }
