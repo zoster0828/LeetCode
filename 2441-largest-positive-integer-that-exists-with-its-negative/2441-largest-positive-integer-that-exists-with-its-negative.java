c13a10437510442255d450650a7e7c73421b0edc
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-05-02 18:09:56 +0900

    Time: 1 ms (100.00%), Space: 44.9 MB (43.00%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.java
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.java
@@ -3,11 +3,14 @@ class Solution {
         boolean[] candidates = new boolean[2001];
         
         int max = -1 ;
-        for(int i = 0 ; i < nums.length ; i++) {
-            int absolute = Math.abs(nums[i]);            
+        for(int i = 0 ; i < nums.length ; i++) {            
+            int absolute = Math.abs(nums[i]);   
+            
+            if(absolute < max) continue;
+            
             candidates[nums[i] < 0 ? absolute+1000 : absolute] = true;            
             
-            if(absolute > max && candidates[absolute] && candidates[absolute+1000]) {
+            if(candidates[absolute] && candidates[absolute+1000]) {
                 max = Math.max(absolute, max);
             }
         }
