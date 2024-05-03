18f0b49de39416c0169efaf7d73f4f1bb53848d0
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-05-03 17:41:43 +0900

    Time: 1 ms (78.87%), Space: 41.2 MB (83.58%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        Version versionOne = parse(version1);
+        Version versionTwo = parse(version2);
+
+        return versionOne.compare(versionTwo);
+    }
+
+    private Version parse(String version1) {
+        return new Version(version1.split("\\."));
+    }
+
+    private class Version {
+        int[] depth;
+
+        public Version(String[] split) {
+            depth = new int[split.length];
+
+            for (int i = 0; i < split.length; i++) {
+                depth[i] = Integer.parseInt(split[i]);
+            }
+        }
+
+        public int compare(Version versionTwo) {
+            if(this.depth.length != versionTwo.depth.length) {
+                int i = 0;
+                while(i < Math.min(this.depth.length, versionTwo.depth.length)) {
+                    if(this.depth[i] > versionTwo.depth[i]) return 1;
+                    else if(this.depth[i] < versionTwo.depth[i]) return -1;
+                    i++;
+                }
+
+                for(;i < depth.length ; i++) {
+                    if(depth[i] != 0) return 1;
+                }
+
+                for(;i < versionTwo.depth.length ; i++) {
+                    if(versionTwo.depth[i] != 0) return -1;
+                }
+
+                return 0;
+            }
+
+            int i = 0;
+            for (; i < this.depth.length ; i++) {
+                if(this.depth[i] > versionTwo.depth[i]) return 1;
+                else if(this.depth[i] < versionTwo.depth[i]) return -1;
+            }
+
+            return 0;
+        }
+    }
+}
