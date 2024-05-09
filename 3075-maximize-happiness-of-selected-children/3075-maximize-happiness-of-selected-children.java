8cf74d6c5e73d29927f93d24065d03e8451cbda3
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-05-09 21:45:21 +0900

    Time: 34 ms (97.44%), Space: 61.3 MB (87.76%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
@@ -5,7 +5,9 @@ class Solution {
         int n = happiness.length - 1;
         long result = 0;
         for(int i = 0 ; i < k ; i++) {
-            result += ((happiness[n-i]-i) >= 0 ? (happiness[n-i]-i) : 0);
+            int candidate = happiness[n-i]-i;
+            if(candidate < 0) candidate = 0;
+            result += candidate;
         }
         
         return result;
