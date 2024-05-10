df7b764aa8c14dab5799e7c859330514a5547fbf
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-05-10 17:39:39 +0900

    Time: 486 ms (39.30%), Space: 85.1 MB (29.26%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
         int counts = arr.length * (arr.length-1) / 2;
-        int[][] pairs = new int[counts][2];
+        float[][] pairs = new float[counts][2];
         int left = 0 , right = arr.length-1;
         int po = 0;
         for(int i = 1 ; i < arr.length ; i++) {
             for(int j = 0 ; j < i ; j++) {
-                pairs[po++] = new int[]{arr[j], arr[i]};                
+                pairs[po++] = new float[]{arr[j], arr[i]};                
             }
         }
         
-        Arrays.sort(pairs, (x, y) -> Double.compare((float)x[0] / (float)x[1], (float)y[0] / (float)y[1]));
+        Arrays.sort(pairs, (x, y) -> Double.compare(x[0] / x[1], y[0] / y[1]));
         
-        return pairs[k-1];
+        return new int[]{(int)pairs[k-1][0], (int)pairs[k-1][1]};
     }
 }
