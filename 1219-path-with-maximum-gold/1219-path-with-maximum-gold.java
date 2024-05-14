55df44da818ce077d85fe45afff4699dee5536ad
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-05-14 19:56:32 +0900

    Time: 44 ms (96.05%), Space: 41.8 MB (16.41%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
@@ -11,19 +11,14 @@ class Solution {
         return max;
     }
     
-    void get(int sum, int[][] grid, int i, int j, boolean[][] visited) {
-        if(visited[i][j]) {
-            max = Math.max(max, sum);
-            return ;
-        }
-            
+    void get(int sum, int[][] grid, int i, int j, boolean[][] visited) {            
         sum += grid[i][j];        
         visited[i][j] = true;
         
-        if(i!=0 && grid[i-1][j] != 0) get(sum, grid, i-1, j, visited);
-        if(j!=0 && grid[i][j-1] != 0) get(sum, grid, i, j-1, visited);
-        if(i!=grid.length-1 && grid[i+1][j] != 0) get(sum, grid, i+1, j, visited);
-        if(j!=grid[0].length-1 && grid[i][j+1] != 0) get(sum, grid, i, j+1, visited);
+        if(i!=0 && grid[i-1][j] != 0 && !visited[i-1][j]) get(sum, grid, i-1, j, visited);
+        if(j!=0 && grid[i][j-1] != 0 && !visited[i][j-1]) get(sum, grid, i, j-1, visited);
+        if(i!=grid.length-1 && grid[i+1][j] != 0 && !visited[i+1][j]) get(sum, grid, i+1, j, visited);
+        if(j!=grid[0].length-1 && grid[i][j+1] != 0 && !visited[i][j+1]) get(sum, grid, i, j+1, visited);
         
         visited[i][j] = false;
         max = Math.max(max, sum);
