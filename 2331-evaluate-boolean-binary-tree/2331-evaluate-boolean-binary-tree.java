049c8319b8d3445ccaa935ce9c7224a3d7be7a27
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-05-16 09:10:13 +0900

    Time: 0 ms (100.00%), Space: 44.1 MB (69.21%) - LeetHub

--- 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.java
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.java
@@ -18,6 +18,7 @@ class Solution {
         if(root.val == 0) return false;
         if(root.val == 1) return true;
         if(root.val == 2) return evaluateTree(root.left) || evaluateTree(root.right);
-        return evaluateTree(root.left) && evaluateTree(root.right);
+        if(root.val == 3) return evaluateTree(root.left) && evaluateTree(root.right);
+        throw new RuntimeException("error");
     }
 }
