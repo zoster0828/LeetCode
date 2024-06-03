aea72a52201e81e40d62c4eab336d9348cfcad73
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-06-03 18:30:51 +0900

    Time: 7 ms (51.85%), Space: 45.2 MB (66.78%) - LeetHub

--- 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.java
+++ 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.java
@@ -3,7 +3,9 @@ class Solution {
         int po = 0;
         for(int i = 0 ; i < s.length() ; i++) {
             char c = s.charAt(i);
-            if(po < t.length() && t.charAt(po) == c) {
+            if(po >= t.length()) break;
+            
+            if(t.charAt(po) == c) {
                 po++;
             }
         }
