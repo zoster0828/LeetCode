e7d973c7fa59be1ff3506018eeb68fb07b260089
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-06-04 09:51:44 +0900

    Time: 3 ms (74.7%), Space: 41.9 MB (25.48%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] chars = new int[100];
+        for(int i = 0 ; i < s.length() ; i++) {
+            int n = s.charAt(i) - 65;
+
+            chars[n] ++;
+        }
+
+        int result = 0;
+        boolean odd = false;
+        for(int i = 0 ; i < chars.length ; i ++) {
+            if(chars[i] % 2 ==0) result+=chars[i];
+            else {
+                result+=(chars[i] - 1);
+                odd = true;
+            }
+        }
+
+        return odd ? result + 1 : result;
+    }
+}
