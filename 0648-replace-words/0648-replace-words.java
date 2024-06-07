59e1839a6407da31a637aa0ee438fbc39b02a62c
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-06-07 09:34:29 +0900

    Time: 53 ms (31.53%), Space: 51.7 MB (94.83%) - LeetHub

--- 0648-replace-words/0648-replace-words.java
+++ 0648-replace-words/0648-replace-words.java
@@ -1,16 +1,12 @@
 class Solution {
     public String replaceWords(List<String> dictionary, String sentence) {
-        Collections.sort(dictionary, new Comparator<String>() {
-            public int compare(String s1, String s2) {
-                return Integer.compare(s1.length(), s2.length());
-            }
-        });
-        
+
         String splitted[] = sentence.split("\\s+");
         boolean[] replaced = new boolean[splitted.length];
         for(String str : dictionary) {            
             for(int i = 0 ; i < splitted.length ; i++) {
-                if(!replaced[i] && splitted[i].startsWith(str)) {
+
+                if((str.length() < splitted[i].length() || !replaced[i]) && splitted[i].startsWith(str)) {
                     splitted[i] = str;
                     replaced[i] = true;
                 }
