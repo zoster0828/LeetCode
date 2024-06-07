24006452b811d784f88dcfc0b1ebf9d38541be96
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-06-07 10:06:57 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0648-replace-words/0648-replace-words.java
+++ 0648-replace-words/0648-replace-words.java
@@ -1,18 +1,43 @@
 class Solution {
     public String replaceWords(List<String> dictionary, String sentence) {
+        List<String> newDict = mergeDictionary(dictionary);
 
         String splitted[] = sentence.split("\\s+");
-        boolean[] replaced = new boolean[splitted.length];
-        for(String str : dictionary) {            
+        for(String str : newDict) {            
             for(int i = 0 ; i < splitted.length ; i++) {
 
-                if((str.length() < splitted[i].length() || !replaced[i]) && splitted[i].startsWith(str)) {
+                if(splitted[i].startsWith(str)) {
                     splitted[i] = str;
-                    replaced[i] = true;
                 }
             }
         }
 
         return String.join(" ", splitted);
+    }
+
+    public List<String> mergeDictionary(List<String> words) {
+        List<String> sortedWords = words.stream()
+            .sorted(Comparator.comparingInt(String::length))
+            .collect(Collectors.toList());
+
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < sortedWords.size(); i++) {
+            String currentWord = sortedWords.get(i);
+            boolean isPrefix = false;
+
+            for (int j = 0; j < sortedWords.size(); j++) {
+                if (i != j && sortedWords.get(j).startsWith(currentWord)) {
+                    isPrefix = true;
+                    break;
+                }
+            }
+
+            if (!isPrefix) {
+                result.add(currentWord);
+            }
+        }
+
+        return result;
     }
 }
