13cf4099f643922c1ef6b5c65de0e510520d4a01
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-06-07 10:25:09 +0900

    Time: 56 ms (31.15%), Space: 51.9 MB (93.82%) - LeetHub

--- 0648-replace-words/0648-replace-words.java
+++ 0648-replace-words/0648-replace-words.java
@@ -1,8 +1,10 @@
 class Solution {
     public String replaceWords(List<String> dictionary, String sentence) {
-
+        List<String> sortedDict = dictionary.stream()
+            .sorted(Comparator.comparingInt(String::length))
+            .collect(Collectors.toList());
         String splitted[] = sentence.split("\\s+");
-        for(String str : dictionary) {            
+        for(String str : sortedDict) {            
             for(int i = 0 ; i < splitted.length ; i++) {
 
                 if(splitted[i].startsWith(str)) {
