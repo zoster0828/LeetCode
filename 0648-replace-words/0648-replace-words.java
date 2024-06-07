22febf10eea20d8140a544d362acb4b3611846af
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-06-07 10:08:47 +0900

    Time: 49 ms (33.8%), Space: 52 MB (93.82%) - LeetHub

--- 0648-replace-words/0648-replace-words.java
+++ 0648-replace-words/0648-replace-words.java
@@ -1,9 +1,8 @@
 class Solution {
     public String replaceWords(List<String> dictionary, String sentence) {
-        List<String> newDict = mergeDictionary(dictionary);
 
         String splitted[] = sentence.split("\\s+");
-        for(String str : newDict) {            
+        for(String str : dictionary) {            
             for(int i = 0 ; i < splitted.length ; i++) {
 
                 if(splitted[i].startsWith(str)) {
@@ -13,31 +12,5 @@ class Solution {
         }
 
         return String.join(" ", splitted);
-    }
-
-    public List<String> mergeDictionary(List<String> words) {
-        List<String> sortedWords = words.stream()
-            .sorted(Comparator.comparingInt(String::length))
-            .collect(Collectors.toList());
-
-        List<String> result = new ArrayList<>();
-
-        for (int i = 0; i < sortedWords.size(); i++) {
-            String currentWord = sortedWords.get(i);
-            boolean isPrefix = false;
-
-            for (int j = 0; j < sortedWords.size(); j++) {
-                if (i != j && sortedWords.get(j).startsWith(currentWord)) {
-                    isPrefix = true;
-                    break;
-                }
-            }
-
-            if (!isPrefix) {
-                result.add(currentWord);
-            }
-        }
-
-        return result;
     }
 }
