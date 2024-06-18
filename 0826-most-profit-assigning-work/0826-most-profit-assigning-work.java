4ca6a2489022992aea949d172f17a289a17deb95
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-06-18 22:14:54 +0900

    Time: 664 ms (9.58%), Space: 45.7 MB (39.31%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -1,25 +1,47 @@
 class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        int max = 0;
-        for(int i = 0 ; i < worker.length ; i++) {
-            max = Math.max(max, worker[i]);
+        DifficultyProfit[] difficultyProfits = new DifficultyProfit[difficulty.length];
+
+        for(int i = 0 ; i < difficultyProfits.length ; i++) {
+            difficultyProfits[i] = new DifficultyProfit(difficulty[i], profit[i]);
         }
 
-        int[] array = new int[max+1];
+        Arrays.sort(difficultyProfits, Comparator.comparingInt(x -> x.difficulty));
 
-        for(int i = 0 ; i < difficulty.length ; i++) {
-            int po = difficulty[i];
-            while(po <= max && array[po] < profit[i]) {
-                array[po] = profit[i];
-                po++;
+        int prev = 0;
+        for(int i = 0 ; i < difficultyProfits.length ; i++) {
+            if(difficultyProfits[i].profit < prev) {
+                difficultyProfits[i].profit = prev;
             }
+
+            prev = difficultyProfits[i].profit;
         }
 
         int result = 0;
         for(int i = 0 ; i < worker.length ; i++) {
-            result += array[worker[i]];
+            int po = 0;
+            while(po < difficultyProfits.length && worker[i] >= difficultyProfits[po].difficulty) {
+                if (po + 1 < difficultyProfits.length && worker[i] < difficultyProfits[po + 1].difficulty) {
+                    break;
+                }
+                po++;
+            }
+            if (po == difficultyProfits.length || worker[i] < difficultyProfits[po].difficulty) {
+                po--;
+            }
+            if(po >= 0)
+                result += difficultyProfits[po].profit;
         }
 
         return result;
+    }
+
+    class DifficultyProfit{
+        public int difficulty;
+        public int profit;
+        public DifficultyProfit(int difficulty, int profit) {
+            this.difficulty = difficulty;
+            this.profit = profit;
+        }
     }
 }
