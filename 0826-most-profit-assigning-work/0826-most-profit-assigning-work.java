72572d77fcd076eca6e824b35a990c20295c91ed
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-06-18 22:18:49 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -1,5 +1,13 @@
 class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+        DifficultyProfit[] difficultyProfits = new DifficultyProfit[difficulty.length];
+
+        for(int i = 0 ; i < difficultyProfits.length ; i++) {
+            difficultyProfits[i] = new DifficultyProfit(difficulty[i], profit[i]);
+        }
+
+        Arrays.sort(difficultyProfits, Comparator.comparingInt(x -> x.difficulty));
+
         int[] cache = new int[100001];
 
         int result = 0;
@@ -9,15 +17,26 @@ class Solution {
                 continue;
             }
 
-            for(int i = 0 ; i < difficulty.length ; i ++) {
-                if(difficulty[i] <= w && cache[w] < profit[i]) {
-                    cache[w] = profit[i];                    
+            for(int i = 0 ; i < difficultyProfits.length ; i ++) {
+                if(difficultyProfits[i].difficulty <= w && cache[w] < difficultyProfits[i].profit) {
+                    cache[w] = profit[i];
                 }
+
+                if(difficultyProfits[i].difficulty > w) break;
             }
 
             result += cache[w];
         }
 
         return result;
+    }
+
+    class DifficultyProfit{
+        public int difficulty;
+        public int profit;
+        public DifficultyProfit(int difficulty, int profit) {
+            this.difficulty = difficulty;
+            this.profit = profit;
+        }
     }
 }
