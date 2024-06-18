04a27345bbd6ea2ecadaadef885a8cfd01f65267
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-06-18 22:37:19 +0900

    Time: 4 ms (100%), Space: 45.9 MB (31.08%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -1,23 +1,28 @@
 class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+        
         int max = 0;
-        for(int i = 0 ; i < worker.length ; i++) {
-            max = Math.max(max, worker[i]);
+        for(int i = 0 ; i < difficulty.length ; i++) {
+            max = Math.max(max, difficulty[i]);
         }
 
-        int[] array = new int[max+1];
+        int[] bestProfit = new int[max + 1];
+        for (int i = 0; i < profit.length; i++) {
+            bestProfit[difficulty[i]] = Math.max(bestProfit[difficulty[i]], profit[i]);
+        }
 
-        for(int i = 0 ; i < difficulty.length ; i++) {
-            int po = difficulty[i];
-            while(po <= max && array[po] < profit[i]) {
-                array[po] = profit[i];
-                po++;
+        int maxProfit = 0;
+        for (int i = 0; i < bestProfit.length; i++) {
+            if (bestProfit[i] > maxProfit) {
+                maxProfit = bestProfit[i];
             }
+            bestProfit[i] = maxProfit;
         }
 
         int result = 0;
         for(int i = 0 ; i < worker.length ; i++) {
-            result += array[worker[i]];
+            if(worker[i] >= bestProfit.length) result += bestProfit[max];
+            else result += bestProfit[worker[i]];
         }
 
         return result;
