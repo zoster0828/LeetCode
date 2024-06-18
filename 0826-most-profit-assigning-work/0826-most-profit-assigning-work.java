c448c7a70a5a6f47ff9c315e8214b6f2286ef5f2
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-06-18 22:35:18 +0900

    Time: 2794 ms (5.03%), Space: 44.7 MB (99.75%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -1,42 +1,25 @@
 class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        DifficultyProfit[] difficultyProfits = new DifficultyProfit[difficulty.length];
-
-        for(int i = 0 ; i < difficultyProfits.length ; i++) {
-            difficultyProfits[i] = new DifficultyProfit(difficulty[i], profit[i]);
+        int max = 0;
+        for(int i = 0 ; i < worker.length ; i++) {
+            max = Math.max(max, worker[i]);
         }
 
-        Arrays.sort(difficultyProfits, Comparator.comparingInt(x -> x.difficulty));
+        int[] array = new int[max+1];
 
-        int[] cache = new int[100001];
+        for(int i = 0 ; i < difficulty.length ; i++) {
+            int po = difficulty[i];
+            while(po <= max && array[po] < profit[i]) {
+                array[po] = profit[i];
+                po++;
+            }
+        }
 
         int result = 0;
-        for(int w : worker) {
-            if(cache[w] != 0) {
-                result += cache[w];
-                continue;
-            }
-
-            for(int i = 0 ; i < difficultyProfits.length ; i ++) {
-                if(difficultyProfits[i].difficulty <= w && cache[w] < difficultyProfits[i].profit) {
-                    cache[w] = profit[i];
-                }
-
-                if(difficultyProfits[i].difficulty > w) break;
-            }
-
-            result += cache[w];
+        for(int i = 0 ; i < worker.length ; i++) {
+            result += array[worker[i]];
         }
 
         return result;
-    }
-
-    class DifficultyProfit{
-        public int difficulty;
-        public int profit;
-        public DifficultyProfit(int difficulty, int profit) {
-            this.difficulty = difficulty;
-            this.profit = profit;
-        }
     }
 }
