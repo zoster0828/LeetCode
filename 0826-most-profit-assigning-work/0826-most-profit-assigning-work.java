51083ecf9342ce81baa808e1708e9742ee9764c5
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-06-18 22:18:04 +0900

    Time: 1088 ms (5.03%), Space: 46.5 MB (6.63%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -1,47 +1,23 @@
 class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        DifficultyProfit[] difficultyProfits = new DifficultyProfit[difficulty.length];
-
-        for(int i = 0 ; i < difficultyProfits.length ; i++) {
-            difficultyProfits[i] = new DifficultyProfit(difficulty[i], profit[i]);
-        }
-
-        Arrays.sort(difficultyProfits, Comparator.comparingInt(x -> x.difficulty));
-
-        int prev = 0;
-        for(int i = 0 ; i < difficultyProfits.length ; i++) {
-            if(difficultyProfits[i].profit < prev) {
-                difficultyProfits[i].profit = prev;
-            }
-
-            prev = difficultyProfits[i].profit;
-        }
+        int[] cache = new int[100001];
 
         int result = 0;
-        for(int i = 0 ; i < worker.length ; i++) {
-            int po = 0;
-            while(po < difficultyProfits.length && worker[i] >= difficultyProfits[po].difficulty) {
-                if (po + 1 < difficultyProfits.length && worker[i] < difficultyProfits[po + 1].difficulty) {
-                    break;
+        for(int w : worker) {
+            if(cache[w] != 0) {
+                result += cache[w];
+                continue;
+            }
+
+            for(int i = 0 ; i < difficulty.length ; i ++) {
+                if(difficulty[i] <= w && cache[w] < profit[i]) {
+                    cache[w] = profit[i];                    
                 }
-                po++;
             }
-            if (po == difficultyProfits.length || worker[i] < difficultyProfits[po].difficulty) {
-                po--;
-            }
-            if(po >= 0)
-                result += difficultyProfits[po].profit;
+
+            result += cache[w];
         }
 
         return result;
-    }
-
-    class DifficultyProfit{
-        public int difficulty;
-        public int profit;
-        public DifficultyProfit(int difficulty, int profit) {
-            this.difficulty = difficulty;
-            this.profit = profit;
-        }
     }
 }
