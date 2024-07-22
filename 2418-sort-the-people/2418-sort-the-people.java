9cd893f98177a96437dd6b353e43ef6e3a7b13f8
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-07-22 23:33:03 +0900

    Time: 9 ms (53.75%), Space: 45.6 MB (10.15%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.java
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -1,30 +1,22 @@
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
-        // 이름과 키를 쌍으로 저장할 클래스 정의
-        class Person {
-            String name;
-            int height;
-            
-            Person(String name, int height) {
-                this.name = name;
-                this.height = height;
+        int n = names.length;
+        String[] sorted = new String[100001];
+
+        for(int i = 0 ; i < n ; i++) {
+            sorted[heights[i]] = names[i];
+        }
+
+
+        String result[] = new String[n];
+        int i = 0;
+        for(int j = sorted.length -1 ; j >= 0 && i < n ; j--) {
+            if(sorted[j] != null) {
+                result[i] = sorted[j];
+                i++;
             }
         }
-        
-        Person[] people = new Person[names.length];
-        for (int i = 0; i < names.length; i++) {
-            people[i] = new Person(names[i], heights[i]);
-        }
-        
-        // 내림차순 정렬
-        Arrays.sort(people, (a, b) -> b.height - a.height);
-        
-        // 정렬된 이름 배열 생성
-        String[] sortedNames = new String[names.length];
-        for (int i = 0; i < people.length; i++) {
-            sortedNames[i] = people[i].name;
-        }
-        
-        return sortedNames;
+
+        return result;
     }
 }
