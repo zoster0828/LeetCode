00d3ab95c20cf6f38de798ef92274a08d6c80715
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-07-25 10:02:16 +0900

    Time: 5 ms (96.08%), Space: 56.4 MB (35.74%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,55 +1,24 @@
 class Solution {
     public int[] sortArray(int[] nums) {        
-        mergeSort(nums, 0, nums.length-1);
-
-        return nums;
-    }
-
-    void mergeSort(int[] array, int left, int right) {
-        if (left < right) {
-            int middle = left + (right - left) / 2;
-
-            mergeSort(array, left, middle);
-            mergeSort(array, middle + 1, right);
-
-            merge(array, left, middle, right);
+        int range[] = new int[100001];
+        
+        for(int num : nums) {
+            range[num+50000]++;
         }
-    }
+        int result[] = new int[nums.length];
+        int po = 0;
+        for(int i = 0 ; i < range.length ; i++) {
+            while(range[i] > 0) {
+                result[po] = i-50000;
+                range[i]--;
+                po++;
+            }            
 
-    void merge(int[] array, int left, int middle, int right) {
-        int n1 = middle - left + 1;
-        int n2 = right - middle;
-
-        int[] leftArray = new int[n1];
-        int[] rightArray = new int[n2];
-
-        System.arraycopy(array, left, leftArray, 0, n1);
-        System.arraycopy(array, middle + 1, rightArray, 0, n2);
-
-        int i = 0, j = 0;
-        int k = left;
-
-        while (i < n1 && j < n2) {
-            if (leftArray[i] <= rightArray[j]) {
-                array[k] = leftArray[i];
-                i++;
-            } else {
-                array[k] = rightArray[j];
-                j++;
+            if(po == result.length) {
+                break;
             }
-            k++;
         }
 
-        while (i < n1) {
-            array[k] = leftArray[i];
-            i++;
-            k++;
-        }
-
-        while (j < n2) {
-            array[k] = rightArray[j];
-            j++;
-            k++;
-        }
+        return result;
     }
 }
