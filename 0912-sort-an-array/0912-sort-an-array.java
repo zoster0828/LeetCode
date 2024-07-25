11909d9bb29c5d1f179a8bba1625cc14ac8a49ca
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-07-25 09:57:18 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,8 +1,37 @@
 class Solution {
-    public int[] sortArray(int[] nums) {
-        
-        Arrays.sort(nums);
-        
+    public int[] sortArray(int[] nums) {        
+        quickSort(nums, 0, nums.length-1);
+
         return nums;
+    }
+
+    void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int pi = partition(array, low, high);
+
+            quickSort(array, low, pi - 1);
+            quickSort(array, pi + 1, high);
+        }
+    }
+
+    int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = (low - 1);
+
+        for (int j = low; j < high; j++) {
+            if (array[j] <= pivot) {
+                i++;
+
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+
+        return i + 1;
     }
 }
