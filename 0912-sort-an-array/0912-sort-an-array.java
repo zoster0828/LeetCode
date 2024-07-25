bcd8d4aafb277213098bffb83d0cc0fdc95f142d
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-07-25 09:58:28 +0900

    Time: 1949 ms (5.02%), Space: 59.9 MB (5.27%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -7,31 +7,31 @@ class Solution {
 
     void quickSort(int[] array, int low, int high) {
         if (low < high) {
-            int pi = partition(array, low, high);
-
-            quickSort(array, low, pi - 1);
-            quickSort(array, pi + 1, high);
+            int[] pivots = partition(array, low, high);
+            quickSort(array, low, pivots[0] - 1);
+            quickSort(array, pivots[1] + 1, high);
         }
     }
 
-    int partition(int[] array, int low, int high) {
+    int[] partition(int[] array, int low, int high) {
         int pivot = array[high];
-        int i = (low - 1);
-
-        for (int j = low; j < high; j++) {
-            if (array[j] <= pivot) {
-                i++;
-
-                int temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
+        int i = low - 1, j = low, k = high;
+        
+        while (j <= k) {
+            if (array[j] < pivot) {
+                swap(array, ++i, j++);
+            } else if (array[j] > pivot) {
+                swap(array, j, k--);
+            } else {
+                j++;
             }
         }
+        return new int[]{i + 1, k};
+    }
 
-        int temp = array[i + 1];
-        array[i + 1] = array[high];
-        array[high] = temp;
-
-        return i + 1;
+    void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
