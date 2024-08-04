8b673f61a6d5ec19e9e3725518755242a1888f86
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-08-04 14:01:25 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int rangeSum(int[] nums, int n, int left, int right) {
+        int totalSubarrays = nums.length * (nums.length + 1) / 2;
+        int[] subarraySums = new int[totalSubarrays];
+        int index = 0;
+
+        for (int start = 0; start < nums.length; start++) {
+            int currentSum = 0;
+            for (int end = start; end < nums.length; end++) {
+                currentSum = (currentSum + nums[end]) % 100000007 ;
+                subarraySums[index++] = currentSum ;
+            }
+        }
+        Arrays.sort(subarraySums);
+
+        int result = 0 ;
+        for(int i = left-1 ; i < right ; i++) {
+            result = (result + subarraySums[i]) % 100000007;
+        }
+        return result;
+    }
+}
