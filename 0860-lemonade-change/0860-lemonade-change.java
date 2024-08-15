9ebba492e3319d4cb54c8cb0e16ffdcd13284e61
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-08-15 14:11:31 +0900

    Time: 2 ms (95.27%), Space: 55.3 MB (95.41%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.java
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -1,26 +1,30 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        int count = 0;
-        int count2 = 0;
-        
-        for(int bill : bills) {
-            if(bill == 5) count++;
-            if(bill == 10) {
-                count--;
-                count2++;
-            }
-            if(bill == 20) {
-                if(count2 != 0) {
-                    count--;
-                    count2--;
-                } else {
-                    count-=3;
-                }
-            }
 
-            if(count < 0 || count2 < 0) return false;
+        int five = 0, ten = 0;
+
+        for(int bill: bills) {
+
+            if( bill == 5)
+                five++;
+            else if( bill == 10) {
+                if( five >= 1) {
+                    five--;
+                    ten++;
+                }else
+                    return false;
+
+            }else {
+                if(five >= 1 && ten >= 1) {
+                    ten--;
+                    five--;
+                }else if( five >= 3)
+                    five -= 3;
+                else
+                    return false;
+            }
         }
-
         return true;
+        
     }
 }
