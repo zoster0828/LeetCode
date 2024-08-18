92cba7a23c2923a30faf37cd5aabf07a421751f2
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-08-18 22:12:14 +0900

    Time: 2 ms (91.97%), Space: 40.9 MB (94.14%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.java
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -1,21 +1,17 @@
 class Solution {
     public int nthUglyNumber(int n) {                
-        PriorityQueue<Long> queue = new PriorityQueue();        
-        queue.add(1L);
-        int count = 0;
-        Long result = -1L;
-        Set<Long> checked = new HashSet();
-        while(count++ < n) {            
-            result = queue.poll();
-            for(int multi : new int[]{2,3,5}){
-                if(!checked.contains(result * multi)){
-                    queue.add(result * multi);
-                    checked.add(result * multi);
-                }
-            }
-                
+        int num[] = new int[n];
+        num[0] = 1;
+        int i2 = 0;
+        int i3 = 0;
+        int i5 = 0;
+        for(int i = 1 ; i < n ; i++) {
+            num[i] = Math.min(Math.min(num[i2]*2, num[i3]*3), num[i5]*5);
+            if(num[i] == num[i2]*2) i2++;
+            if(num[i] == num[i3]*3) i3++;
+            if(num[i] == num[i5]*5) i5++;
         }
 
-        return result.intValue();
+        return num[n-1];
     }
 }
