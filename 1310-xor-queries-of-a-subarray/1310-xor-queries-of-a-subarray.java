58f02480e7a6b55cadcf28ed8d56303495555fc1
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-09-13 11:57:19 +0900

    Time: 234 ms (24.77%), Space: 57.2 MB (69.91%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.java
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[] xorQueries(int[] arr, int[][] queries) {
+        Map<String, Integer> map = new HashMap();
         int n = queries.length;
         int[] result = new int[n];
 
@@ -7,8 +8,15 @@ class Solution {
             if(queries[i][0] == queries[i][1]) {
                 result[i] = arr[queries[i][0]];
             } else {
-                for(int j = queries[i][0] ; j <= queries[i][1] ; j++)
-                    result[i] ^= arr[j];
+                String key = queries[i][0] +""+ queries[i][1];
+                if(map.containsKey(key)) {
+                    result[i] = map.get(key);
+                } else {
+                    for(int j = queries[i][0] ; j <= queries[i][1] ; j++) {
+                        result[i] ^= arr[j];
+                    }
+                    map.put(key, result[i]);                    
+                }
             }
         }        
 
