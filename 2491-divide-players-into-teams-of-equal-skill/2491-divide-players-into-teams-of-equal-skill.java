e3b4eb0199c3a6f834c149f0200e04d291c7d377
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-10-04 19:41:38 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.java
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.java
@@ -1,14 +1,28 @@
 class Solution {
     public long dividePlayers(int[] skill) {
-        Arrays.sort(skill);
+        int n = skill.length;
 
-        int num = skill[0] + skill[skill.length - 1];
-        long result = skill[0] * skill[skill.length - 1];
-        for(int i = 1 ; i < skill.length / 2 ; i++) {
-            if(num == skill[i] + skill[skill.length - i - 1]) {
-                result += skill[i] * skill[skill.length - i - 1];
-            }else {
-                return -1;
+        int[] p = new int[1001];
+        int sum = 0;
+        for(int i : skill) {
+            p[i]++;
+            sum += i;
+        }
+
+        if(sum % (n/2) != 0) return -1;
+
+        int avg = sum / ((n) / 2);
+
+        long result = 0;
+        for(int i = 1 ; i < 1001 ; i++) {
+            if(p[i] != 0) {
+                if (p[i] == p[avg - i]) {
+                    result += (i * (avg - i)) * ((i==(avg-i)) ? (p[i]/2) : p[i]);
+                    p[i] = 0;
+                    p[avg - i] = 0;
+                } else {
+                    return -1;
+                }
             }
         }
 
