c2c29e5fbf2643958273ef8773c08d70a9dbc251
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-11-06 11:56:14 +0900

    Time: 5 ms (44.29%), Space: 43.8 MB (97.48%) - LeetHub

--- 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.java
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.java
@@ -2,16 +2,10 @@ class Solution {
     public boolean canSortArray(int[] nums) {
         int n = nums.length;
 
-        int[] oneCount = new int[257];
-        
-        for(int i = 0 ; i < n ; i++) {
-            oneCount[nums[i]] = countOne(nums[i]);
-        }
-
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - 1 - i; j++) {
                 if (nums[j] > nums[j + 1]) {
-                    if(oneCount[nums[j]] != oneCount[nums[j+1]]) return false;
+                    if(Integer.bitCount(nums[j]) != Integer.bitCount(nums[j+1])) return false;
 
                     int temp = nums[j];
                     nums[j] = nums[j + 1];
