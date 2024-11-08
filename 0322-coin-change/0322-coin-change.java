3ef365c5f4fe366aaa613a76a7b4fb6dbd5b7a25
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-11-09 00:23:18 +0900

    Time: 11 ms (96.21%), Space: 44 MB (89.09%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -8,10 +8,12 @@ class Solution {
         dp[0] = 0;
 
         for(int i = 1 ; i < amount+1 ; i++) {
+            int min = 10001;
             for(int j = 0 ; j < n ; j++) {
                 if(i-coins[j] >= 0)
-                    dp[i] = Math.min(dp[i], dp[i-coins[j]] + 1);
+                    min = Math.min(min, dp[i-coins[j]] + 1);
             }
+            dp[i] = min;
         }
 
         return dp[amount] != 10001 ? dp[amount] : -1;
