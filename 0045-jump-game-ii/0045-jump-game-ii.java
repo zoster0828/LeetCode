eb87c6858c96cd306ea1806ff39bfe5df33d9ad7
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-11-09 11:00:58 +0900

    Time: 1 ms (99.27%), Space: 45.2 MB (44.51%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -3,19 +3,16 @@ class Solution {
         int n = nums.length;
         if(n == 1) return 0;
 
-        int target = n - 1;
-        int i = 0;
         int result = 0;
-        while(target != 0) {
-            if(target <= i+nums[i]) {
-                target = i;
+        int max = 0;
+        int currEnd = 0;
+        for(int i = 0 ; i < n - 1 ; i++) {
+            max = Math.max(i+nums[i], max);
+            if(i == currEnd) {
                 result++;
-                i = 0;
-                continue;
+                currEnd = max;
             }
-            i++;
         }
-
         return result;
     }
 }
