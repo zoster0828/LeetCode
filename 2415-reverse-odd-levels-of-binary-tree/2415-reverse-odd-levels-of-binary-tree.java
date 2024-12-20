2e77dd99f33fae3636a2dfe02bd36a0eb03e8fb1
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2024-12-20 21:41:45 +0900

    Time: 0 ms (100%), Space: 48.3 MB (68.82%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.java
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.java
@@ -1,35 +1,21 @@
 class Solution {
     public TreeNode reverseOddLevels(TreeNode root) {
-        if (root == null) {
-            return root;
-        }
-
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        int level = 0;
-        while (!q.isEmpty()) {
-            int size = q.size();
-            while (size-- > 0) {
-                TreeNode node = q.poll();
-                if (node.left != null) q.offer(node.left);
-                if (node.right != null) q.offer(node.right);
-            }
-
-            level++;
-            if (level % 2 == 1 && !q.isEmpty()) {
-                int[] nums = new int[q.size()];
-                int i = 0;
-                for (TreeNode node : q) {
-                    nums[i++] = node.val;
-                }
-
-                int j = q.size() - 1;
-                for (TreeNode node : q) {
-                    node.val = nums[j--];
-                }
-            }
-        }
-
+        // We call the function from lvl 0, and everything starts from lvl 1
+        traverse(root.left, root.right, 1);
         return root;
+    }
+
+    public void traverse(TreeNode node1, TreeNode node2, int lvl) {
+        if (node1 == null || node2 == null) {
+            return;
+        }
+        if (lvl % 2 == 1){
+            int temp = node1.val;
+            node1.val = node2.val;
+            node2.val = temp;
+        }
+
+        traverse(node1.left, node2.right, lvl + 1);
+        traverse(node1.right, node2.left, lvl + 1);
     }
 }
