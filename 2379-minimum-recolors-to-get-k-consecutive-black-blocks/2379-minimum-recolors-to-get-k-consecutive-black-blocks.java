a0f136a037a75ab5d3eac063192729a70466b14a
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2025-03-08 13:07:57 +0900

    Time: 1 ms (92.1%), Space: 41.7 MB (40.6%) - LeetHub

--- 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -9,14 +9,16 @@ class Solution {
         }
         
         int min = count;
-        for(int i = 1 ; i < n - k +1 ; i++) {
-            if(count == k) return 0;
+        for(int i = 1 ; i < n - k +1 ; i++) {            
             if(blocks.charAt(i-1) != blocks.charAt(i-1+k)) {
                 if(blocks.charAt(i-1) == 'W') count++;
                 else count--;
                 min = Math.max(count, min);
             }
-           
+            if(min > k) {
+                min = k;
+                if(min==k) return 0;
+            }
         }
 
         return k-min;
