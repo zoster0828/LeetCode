4b7518bd60ca33c491873e4656238a036e277c11
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2025-05-25 11:14:32 +0900

    Time: 7 ms (99.59%), Space: 60.6 MB (17.28%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int longestPalindrome(String[] words) {
+        int[][] wordCount = new int[26][26];
+        for(String word : words) {
+            char c1 = word.charAt(0);
+            char c2 = word.charAt(1);
+            wordCount[c1-'a'][c2-'a']++;
+        }
+
+        int result = 0;        
+        
+        for(int i = 0 ; i < 26 ; i++) {
+            for(int j = 0 ; j < 26 ; j++) {
+                if(i==j) continue;
+                result += Math.min(wordCount[i][j], wordCount[j][i]);
+            }
+        }
+
+        boolean a = true;
+        for(int i = 0 ; i < 26 ; i++) {
+            if(wordCount[i][i] > 1) {
+                if(wordCount[i][i] % 2 == 0) {
+                    result += wordCount[i][i];
+                } else {
+                    result += wordCount[i][i] -1;              
+                }
+            }
+
+            if(wordCount[i][i] % 2 != 0 && a) {
+                result += 1;
+                a = false;
+            }
+        }
+
+        return result * 2;
+    }
+}
