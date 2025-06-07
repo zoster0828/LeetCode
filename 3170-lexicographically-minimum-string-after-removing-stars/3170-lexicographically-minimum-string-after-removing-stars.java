2d0bd08174a31b6a680b68dcbe9f01e2ad64788d
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2025-06-07 09:29:46 +0900

    Time: 299 ms (29.09%), Space: 47 MB (72.73%) - LeetHub

--- 3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.java
+++ 3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.java
@@ -1,36 +1,26 @@
-
 class Solution {
-    public String clearStars(String s){
-        int n = s.length();
-        
-        List<List<Integer>> buckets = new ArrayList<>();
-        
-        for(int i = 0; i < 26; i++){
-            buckets.add(new ArrayList<>());
-        }
-        
-        boolean[] removed = new boolean[n];
-        
-        for (int i = 0 ; i < n ; i++){
-            if(s.charAt(i) == '*'){
-                removed[i] = true;
-                for (int j = 0; j < 26; j++){
-                    if (!buckets.get(j).isEmpty()){
-                        removed[buckets.get(j).get(buckets.get(j).size() - 1)] = true;
-                        buckets.get(j).remove(buckets.get(j).size() - 1);
+    public String clearStars(String s) {
+        Map<Character, Stack<Integer>> m = new HashMap();
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == '*') {
+                for(char ch = 'a'; ch <= 'z'; ch++) {
+                    if(m.get(ch) != null && !m.get(ch).isEmpty()){
+                        m.get(ch).pop();
                         break;
                     }
                 }
-            } 
-            else{
-                buckets.get(s.charAt(i) - 'a').add(i);
+            } else {
+                if(!m.containsKey(c))
+                    m.put(c, new Stack());
+                m.get(c).push(i);
             }
         }
-        
-        StringBuilder ans = new StringBuilder();
-        for(int i = 0 ; i < n ; i++){
-            if(!removed[i]) ans.append(s.charAt(i));
+        char[] ans = new char[s.length()];
+        for(Map.Entry<Character, Stack<Integer>> e : m.entrySet()) {
+            while(!e.getValue().isEmpty())
+                ans[e.getValue().pop()] = e.getKey();
         }
-        return ans.toString();
+        return new String(ans).replaceAll("\0", "");
     }
 }
