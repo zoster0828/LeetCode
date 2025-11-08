60ae2974d322e69f19b9787f67eae0747757b6a4
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2025-11-08 21:07:51 +0900

    Time: 3 ms (84.5%), Space: 55.5 MB (30.18%) - LeetHub

--- 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.java
+++ 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.java
@@ -1,34 +1,17 @@
 class Solution {
-    public int numEquivDominoPairs(int[][] dominoes) {
+    public int numEquivDominoPairs(int[][] dominoes) {        
         int result = 0;
-        boolean[] checked = new boolean[dominoes.length];
-        int next = 0;
-        boolean change = true;
-        for(int i = next ; i < dominoes.length - 1 ; i++) {
-            if(checked[i]) {
-                continue;
-            }
-            change = true;
-            int now = 0;
-            for(int j = i+1 ; j < dominoes.length ; j++) {
-                if((dominoes[i][0] == dominoes[j][0] && dominoes[i][1] == dominoes[j][1]) ||
-                        (dominoes[i][0] == dominoes[j][1] && dominoes[i][1] == dominoes[j][0])) {
-                    now++;
-                    checked[j] = true;
-                } else {
-                    if(change){
-                        next = i;
-                        change = false;
-                    }
-                }
-            }
-            if(now != 0){
-                result += ((now+1)*now) / 2;
-            }
+        int n = dominoes.length;
+        int arr[] = new int[100];
+        for(int i = 0 ; i < n ; i++) {
+            int a = dominoes[i][0]*10 + dominoes[i][1];
+            int b = dominoes[i][1]*10 + dominoes[i][0];
+            arr[Math.max(a,b)]++;
         }
 
-
-
+        for(int i = 0 ; i < 100 ; i++) {
+            result += arr[i] * (arr[i] -1) /2;            
+        }
 
         return result;
     }
