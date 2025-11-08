d7d4513753df8ddf124b7b9dea962bdecec2624e
zoster0828 <59214428+zoster0828@users.noreply.github.com>
2025-11-08 21:15:33 +0900

    Time: 4 ms (73.14%), Space: 55.4 MB (31.01%) - LeetHub

--- 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.java
+++ 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.java
@@ -1,16 +1,16 @@
 class Solution {
-    public int numEquivDominoPairs(int[][] dominoes) {        
+    public int numEquivDominoPairs(int[][] dominoes) {
+        int[] count = new int[100];
         int result = 0;
-        int n = dominoes.length;
-        int arr[] = new int[100];
-        for(int i = 0 ; i < n ; i++) {
-            int a = dominoes[i][0]*10 + dominoes[i][1];
-            int b = dominoes[i][1]*10 + dominoes[i][0];
-            arr[Math.max(a,b)]++;
-        }
 
-        for(int i = 0 ; i < 100 ; i++) {
-            result += arr[i] * (arr[i] -1) /2;            
+        for (int[] d : dominoes) {
+            // 정렬된 형태로 key 통일 (작은 수가 앞)
+            int a = Math.min(d[0], d[1]);
+            int b = Math.max(d[0], d[1]);
+            int key = a * 10 + b;
+
+            result += count[key]; // 기존 동일 도미노 수만큼 pair 추가
+            count[key]++;         // 자기 자신 카운트 증가
         }
 
         return result;
